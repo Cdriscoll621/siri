64d253f8931263740a4a2bf302ed142477a9e432
Charlotte Driscoll <charlotte.d.driscoll@icloud.com>
2018-01-28 16:17:30 -0600

    added twitter keys

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log("Keys loaded");
+
+var twitterKeys = {
+    consumer_key: "nypokJt84eUtLJtc1ZqWLwNVm",
+    consumer_secret: "mmp6lFhiwvfT9ynItYaWoDxiRGZhTqmasxNi69MDDC6LygVFXQ",
+    access_token_key: "926919024977563648-wpw4cMNgg4GwcnaWFIVkBaMjAsTVOun",
+    access_token_secret: "\t3j0l030lBQzn6UcCQMeNKNlcnOfgRDQxCvFnwYlyXwD8n"
+};
+
+module.exports = twitterKeys;
